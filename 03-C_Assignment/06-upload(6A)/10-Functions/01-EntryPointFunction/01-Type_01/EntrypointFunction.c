b8cc8bbe2cd6dcf88349fcd6d789db0462dcb9d8
prasad sunil korade <prasadkorade5818@gmail.com>
2026-08-20 15:38:34 +0530

    Assignment upload

--- 03-C_Assignment/06-upload(6A)/10-Functions/01-EntryPointFunction/01-Type_01/EntrypointFunction.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> //stdio.h contain declaration of printf()
-
-// entry point function => main() => no return type (void) and no prameters (void), hence no command - line argument
-
-void main(void)
-{
-          //code
-          printf("Hellow World !!!\n");
-}
--- 03-C_Assignment/06-upload(6B)/03-GlobalVariables/02-ExternalGlobalVariables/02-ExternalGlobalVariablesInMultipleFiles/File_02.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void change_count_two(void)
-{
-          //code
-          extern int PK_global_count;
-          PK_global_count = PK_global_count +1;
-          printf("change_count_two() : value of PK_global_count in File_02 = %d\n", PK_global_count);
-}
